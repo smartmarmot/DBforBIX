c7693f3281789b2b3f42a3eb09695e456585d593
U-ADDEV\adallevacche <adallevacche@bnxwnjdnxgap16.addev.bloomberg.com>
2017-09-08 06:12:17 -0400

    created the new package based as suggested by KJt77

--- src/com/smartmarmot/common/Constants.java
+++ src/com/smartmarmot/common/Constants.java
@@ -20,7 +20,7 @@
 package com.smartmarmot.common;
 
 public class Constants {
-	private static final String VERSION = "Version 2.4-beta";
+	private static final String VERSION = "Version 3.0-beta";
 	public static final String BANNER = Constants.PROJECT_NAME + " " + VERSION;
 	private static final String PROJECT_NAME = "DBforBix";
 }
